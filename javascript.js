3f3658aa8980e9da20da1e69fce08a69700adcaf
Rafael <rafaelcardosonicolau2003@hotmail.com>
2022-02-03 04:42:08 +0000

    a

--- javascript.js
+++ javascript.js
@@ -1,33 +1,45 @@
+let player=0;
+let computador=0;
+for(x=0;x<5;x++){
 function computerPlay(){
    let a= Math.floor(Math.random()*3);
     const options = ["Rock", "Paper", "Scissors"];
     return options[a]
 }
-
-function playRound(playerSelection,computerSelection){
-    if((playerSelection == 'rock' && computerSelection == 'scissors')||(playerSelection == 'scissors' && computerSelection == 'paper')||(playerSelection == 'paper' && computerSelection == 'rock')){
-        return "Player Wins"
+function game(playerSelection,computerSelection){
+    
+    
+    function playRound(){
+    
+    
+        if((playerSelection == 'rock' && computerSelection == 'scissors')||(playerSelection == 'scissors' && computerSelection == 'paper')||(playerSelection == 'paper' && computerSelection == 'rock')){
+            player++
+            return "Player Wins"
     }if((computerSelection == 'rock' && playerSelection == 'scissors')||(computerSelection == 'scissors' && playerSelection == 'paper')||(computerSelection == 'paper' && playerSelection == 'rock')){
+        computador++
         return "Computer Wins"
     }else{
         return "Draw"
+        }
     }
+    return playRound()
 
 }
-function game(playRound){
-    let player=0
-    let computer=0
-    for(x=1;x<=5;x++){
-        winner=playRound()
-        if(winner=='Player Wins'){
-            player+=1
-        }else if(winner=='Computer Wins'){
-            computer+=1
-        } 
-    }
 
-}
-const playerSelection=prompt("Choose paper,rock or scissors!").toLowerCase();
+
+
 const computerSelection=computerPlay().toLowerCase();
-console.log(computerSelection);
-console.log(playRound(playerSelection,computerSelection))
+const playerSelection=prompt("Choose paper,rock or scissors!").toLowerCase();
+console.log(computerSelection)
+
+game(playerSelection,computerSelection)
+console.log(player);
+console.log(computador);
+}
+if(computador>player){
+    console.log("Computer Wins")
+}else if(player>computador){
+    console.log("Player Wins")
+}else{
+    console.log("Draw")
+}
