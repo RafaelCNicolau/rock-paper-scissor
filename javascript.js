83f454ecb56380df5655b1240a4f4706d9343be0
Rafael <rafaelcardosonicolau2003@hotmail.com>
2022-02-03 03:53:43 +0000

    jo

--- javascript.js
+++ javascript.js
@@ -5,13 +5,29 @@ function computerPlay(){
 }
 
 function playRound(playerSelection,computerSelection){
-    if((playerSelection=='rock' && computerSelection=='scissors')||(playerSelection=='scissors'&&computerSelection=='paper')||(playerSelection=='paper'&&computerSelection=='rock')){
+    if((playerSelection == 'rock' && computerSelection == 'scissors')||(playerSelection == 'scissors' && computerSelection == 'paper')||(playerSelection == 'paper' && computerSelection == 'rock')){
         return "Player Wins"
+    }if((computerSelection == 'rock' && playerSelection == 'scissors')||(computerSelection == 'scissors' && playerSelection == 'paper')||(computerSelection == 'paper' && playerSelection == 'rock')){
+        return "Computer Wins"
+    }else{
+        return "Draw"
     }
 
 }
+function game(playRound){
+    let player=0
+    let computer=0
+    for(x=1;x<=5;x++){
+        winner=playRound()
+        if(winner=='Player Wins'){
+            player+=1
+        }else if(winner=='Computer Wins'){
+            computer+=1
+        } 
+    }
 
+}
 const playerSelection=prompt("Choose paper,rock or scissors!").toLowerCase();
-const computerSelection=computerPlay().toLocaleLowerCase();
-console.log(computerPlay());
-console.log(playRound())
+const computerSelection=computerPlay().toLowerCase();
+console.log(computerSelection);
+console.log(playRound(playerSelection,computerSelection))
